17dbe3be9201d79e2bcb17985825259258f3fc2d
Kateryna Nosova <nosova.kateryna@gmail.com>
2023-12-21 10:26:58 -0800

    Add dnd for main page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,9 @@
-import reactLogo from './assets/react.svg'
-
-import viteLogo from '/vite.svg'
 import './App.scss'
 
 import Home from './pages/Home/Home';
 import Form from './pages/Form/Form';
 import Header from './Components/Header/Header';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import { HTML5Backend } from 'react-dnd-html5-backend';
-import { DndProvider } from 'react-dnd';
 
 function App() {
   return (
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,13 @@
 import Header from "../../Components/Header/Header";
 import MainImageBlock from "../../Components/MainImageBlock/MainImageBlock";
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { DndProvider } from 'react-dnd';
 
 const Home = () => {
-
     return (
-        <>
-            <MainImageBlock></MainImageBlock>
-        </>
+        <DndProvider backend={HTML5Backend}>
+            <MainImageBlock/>
+        </DndProvider>
     )
 }
 
