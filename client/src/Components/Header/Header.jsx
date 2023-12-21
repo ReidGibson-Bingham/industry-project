c85bccdcc46d22a1eeeecd20a56f53fe7b60b323
Afnan <afnanyb@gmail.com>
2023-12-21 10:34:01 -0800

    header style update

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -25,32 +25,40 @@ const Header = () => {
     <header className="header">
       <section className="header__logo-container right">
         <img className="header__logo" src={logo} />
-        <ul className="header__items">
-          <li> MEN</li>
-          <li> WOMEN</li>
-          <li> EXPLORE</li>
-          <li> GIFT GUIDE</li>
-          <li> SIZE GUIDE</li>
-        </ul>
-      </section>
-
-      <section className="header__pages">
-        <NavLink
-          to="/"
-          className={`header__pages-name ${
-            isActiveHome("/") ? "header__pages-name--active" : ""
-          }`}
-        >
-          Home
-        </NavLink>
-        <NavLink
-          to="/form"
-          className={`header__pages-name ${
-            isActiveForm("/form") ? "header__pages-name--active" : ""
-          }`}
-        >
-          Data Input
-        </NavLink>
+        <section className="header__pages">
+          <NavLink
+            to="/"
+            className={`header__pages-name ${
+              isActiveHome("/") ? "header__pages-name--active" : ""
+            }`}
+          >
+            MEN
+          </NavLink>
+          <NavLink
+            to="/form"
+            className={`header__pages-name ${
+              isActiveForm("/form") ? "header__pages-name--active" : ""
+            }`}
+          >
+            WOMEN
+          </NavLink>
+          <NavLink
+            to="/form"
+            className={`header__pages-name ${
+              isActiveForm("/form") ? "header__pages-name--active" : ""
+            }`}
+          >
+            EXPLORE
+          </NavLink>
+          <NavLink
+            to="/form"
+            className={`header__pages-name ${
+              isActiveForm("/form") ? "header__pages-name--active" : ""
+            }`}
+          >
+            SIZE GUIDE
+          </NavLink>
+        </section>
       </section>
     </header>
   );
