0a2b52df30fa4438738641b61d70119a5b7fead2
Kateryna Nosova <nosova.kateryna@gmail.com>
2023-12-21 13:21:56 -0800

    Add Product list jackets

--- client/src/Components/MainImageBlock/MainImageBlock.jsx
+++ client/src/Components/MainImageBlock/MainImageBlock.jsx
@@ -3,8 +3,10 @@ import { useDrag, useDrop } from 'react-dnd';
 
 import model from './../../assets/models/female_model.png';
 import modelLegs from './../../assets/models/female_model_legs.png';
-import purpleJacket from './../../assets/jackets/purple-jacket.png';
-import beigeJacket from './../../assets/jackets/beige-jacket.png';
+import purpleJacketModel1 from './../../assets/jackets/purple-jacket-model-1.png';
+import beigeJacketModel1 from './../../assets/jackets/beige-jacket-model-1.png';
+import yellowJacketModel2 from './../../assets/jackets/yellow-jacket-model-2.png';
+import brownJacketModel2 from './../../assets/jackets/brown-jacket-model-2.png';
 import './MainImageBlock.scss';
 
 const DraggableItem = ({ img }) => {
@@ -13,7 +15,7 @@ const DraggableItem = ({ img }) => {
     });
 
     return (
-        <div ref={drag} style={{ border: '1px solid #000', padding: '8px', margin: '8px', cursor: 'move' }}>
+        <div ref={drag} style={{ border: '1px solid #000', padding: '8px', cursor: 'move' }}>
             <img className="main__left-img" src={img} />
         </div>
     );
@@ -35,49 +37,71 @@ const jackets = [{
     "name": "ATOM JACKET WOMEN'S",
     "description": "Light, versatile synthetic insulated jacket works as a midlayer and standalone. Atom Series: Synthetic insulated midlayers.",
     "price": '$340.00',
+    "image": purpleJacketModel1,
     "colorImages": [{
         color: 'purple',
-        image: purpleJacket,
+        image: purpleJacketModel1,
     }, {
         color: 'beige',
-        image: beigeJacket,
+        image: beigeJacketModel1,
     }],
-    "features": [
-        "Versatile synthetic insulation",
-        "Suitable as a midlayer or standalone jacket",
-        "Sleek and timeless design"
-    ]
+}, {
+    "name": "BETA LT JACKET WOMEN'S",
+    "description": "Full protection in a lightweight (LT), versatile, comfortable GORE-TEX shell. Beta Series: All round mountain apparel. | LT: Lightweight.",
+    "price": '$550.00',
+    "image": yellowJacketModel2,
+    "colorImages": [{
+        color: 'purple',
+        image: yellowJacketModel2,
+    }, {
+        color: 'beige',
+        image: brownJacketModel2,
+    }],
 }];
 
 const MainImageBlock = () => {
     const [droppedItem, setDroppedItem] = useState(null);
+    const [products, setProducts] = useState(jackets);
     const [selectedProduct, setSelectedProduct] = useState(jackets[0]);
-    const [selectedProductColor, setSelectedProductColor] = useState(jackets[0].colorImages[0]);
 
     const onDrop = () => {
         setDroppedItem(selectedProduct);
     }
 
-    const handleSelectColor = (index) => {
-        setSelectedProductColor(selectedProduct.colorImages[index]);
+    const handleSelectColor = ({ productIndex, colorIndex }) => {
+        const newProducts = products.map((product, index) => {
+            const _product = { ...product };
+            
+            if (index === productIndex) {
+                _product.image = products[productIndex].colorImages[colorIndex].image;
+            }
+
+            return _product;
+        })
+        setProducts(newProducts);
+        setSelectedProduct(newProducts[productIndex]);
     };
 
     return (
         <div className="main">
             <div className="main__left">
-                <div className="main__product">
-                    <DraggableItem img={selectedProductColor.image} />
-                    <div className="main__img-colors">
-                        {selectedProduct.colorImages.map((item, index) => (
-                            <img className="main__img-color" src={item.image} onClick={() => handleSelectColor(index)}/>
-                        ))}
-                    </div>
+                <div className="main__product-list">
+                    {products.map((item, index) => (
+                        <div key={index} className="main__product">
+                            <DraggableItem img={item.image} />
+                            <div className="main__img-colors">
+                                {item.colorImages.map((item, colorIndex) => (
+                                    <img key={colorIndex} className="main__img-color" src={item.image} onClick={() => handleSelectColor({ productIndex: index, colorIndex })}/>
+                                ))}
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
             <div className="main__center">
                 <img className="main__img" src={model}/>
                 {droppedItem && <div className="main__img-selected-background"></div>}
-                {droppedItem && <img className="main__img-selected" src={selectedProductColor.image}/>}
+                {droppedItem && <img className="main__img-selected" src={selectedProduct.image}/>}
                 {droppedItem && <img className="main__img-legs" src={modelLegs}/>}
                 <DropTarget onDrop={onDrop} />
             </div>
@@ -86,7 +110,7 @@ const MainImageBlock = () => {
                 <p className="main__right-product-description">{selectedProduct.description}</p>
                 <p className="main__right-product-price">{selectedProduct.price}</p>
                 <p>Your size: <b>S</b></p>
-                <p>Colour: <b>{selectedProductColor.color ?? ''}</b></p>
+                <p>Colour: <b>{selectedProduct.color ?? ''}</b></p>
                 <div className="main__right-size-list">
                     <div className="main__right-size-list-item">
                         XXS
